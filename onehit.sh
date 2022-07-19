4a338c243f8393fc254c7f7f8602e4db25454f9d
cr4sh <54245632+rkhunt3r@users.noreply.github.com>
2022-07-19 16:45:30 +0200

    Add files via upload

--- onehit.sh
+++ onehit.sh
@@ -2,11 +2,11 @@
 
 check_update(){
     changed=0
-    git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
+    git remote update && LC_ALL=C git status -uno | grep -q 'Your branch is behind' && changed=1
         if [ $changed = 1 ]; then
             # git pull
             # echo "\n\e[0m[\e[92mi\e[0m] Need update \n";
-            updates_string="\e[0m[\e[92mUpdate avaiable!\e[0m]"
+            updates_string="\e[0m\e[92mUpdate avaiable!\e[0m"
         else
             # echo "\n\e[0m[\e[92mi\e[0m] No need for update \n"
             updates_string="\e[0m\e[93mNo updates avaiable\e[0m"
@@ -22,7 +22,6 @@ banner(){
 onehit_menu(){
     banner
     printf "| \e[0m\e[96mOneShot UI v1\e[0m | \e[0m\e[95mgithub.com/rkhunt3r/onehit\e[0m | $updates_string |"
-    printf "UPDATE TEST STRING"
 }
 
 check_update
